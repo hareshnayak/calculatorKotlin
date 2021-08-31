75c9867225e5fddec1ef672953fa3c47430a06d3
hareshnayak <sketchharry01@gmail.com>
2021-09-01 00:43:36 +0530

    Added images

--- app/src/main/java/com/hareshnayak/developerdays/MainActivity.kt
+++ app/src/main/java/com/hareshnayak/developerdays/MainActivity.kt
@@ -3,6 +3,7 @@ package com.hareshnayak.developerdays
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -24,20 +25,26 @@ class MainActivity : AppCompatActivity() {
         val ans : TextView = findViewById(R.id.answer)
         val op : TextView = findViewById(R.id.operator)
 
+        val imageCal : ImageView = findViewById(R.id.imageView)
+
         add.setOnClickListener{
             op.text = "+"
+            imageCal.setImageResource(R.drawable.add)
             ans.text = (num1.text.toString().toFloat() + num2.text.toString().toFloat()).toString()
         }
         sub.setOnClickListener{
             op.text = "-"
+            imageCal.setImageResource(R.drawable.sub)
             ans.text = (num1.text.toString().toFloat() - num2.text.toString().toFloat()).toString()
         }
         mul.setOnClickListener{
             op.text = "x"
+            imageCal.setImageResource(R.drawable.multi)
             ans.text = (num1.text.toString().toFloat() * num2.text.toString().toFloat()).toString()
         }
         div.setOnClickListener{
             op.text = "/"
+            imageCal.setImageResource(R.drawable.divide)
             ans.text = (num1.text.toString().toFloat() / num2.text.toString().toFloat()).toString()
         }
     }
